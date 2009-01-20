71ba0631e3f0d50c1b3e19c6282fe362a5d63ee7
Shaun Ruffell <sruffell@digium.com>
2009-01-20 21:23:34 +0000

    Some distributions have backported the hrtimer_get_expires and hrtimer_set_expires functions from 2.6.28.  Therefore, dahdi-linux cannot use the linux version code to determine if these functions are present or not. This patch actively checks for those functions.
    Closes Issue: #14166
    Thanks to bergolth for the patch.
    
    
    git-svn-id: http://svn.asterisk.org/svn/dahdi/linux/trunk@5770 a0bf4364-ded3-4de4-8d8a-66a801d63aff

--- drivers/dahdi/dahdi_dummy.c
+++ drivers/dahdi/dahdi_dummy.c
@@ -89,8 +89,10 @@
 #define USB2420
 #endif
 
-#if defined(USE_HIGHRESTIMER) && ( LINUX_VERSION_CODE < KERNEL_VERSION(2,6,28) )
-/* compatibility with new hrtimer interface */
+#if defined(USE_HIGHRESTIMER) && \
+	(LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 27)) || \
+	(!defined(HAVE_HRTIMER_ACCESSORS))
+/* Compatibility with new hrtimer interface */
 static inline ktime_t hrtimer_get_expires(const struct hrtimer *timer)
 {
 	return timer->expires;
