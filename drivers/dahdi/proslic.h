8b4af89a7583099d5ca1e65ad59940f64e65e5d4
Doug Bailey <dbailey@digium.com>
2009-08-05 14:40:45 +0000

    Change proslic linefeed register setting
    Insure that proslic linefeed register is not transitioned from Active to
    On-Hook Transmission while the channel is off-hook.
    Replaced magic numbers assigned to linefeed associated variables with more
    descriptive constants. 
    
    (issue #15352)
    Reported by: alecdavis
    Patches:
          wctdm_prevent_ohttimer_click.diff3.txt uploaded by dbailey (license 819)
    Tested by: alecdavis, dbailey, vmikhelson
    
    
    
    git-svn-id: http://svn.asterisk.org/svn/dahdi/linux/trunk@6941 a0bf4364-ded3-4de4-8d8a-66a801d63aff

--- drivers/dahdi/proslic.h
+++ drivers/dahdi/proslic.h
@@ -35,6 +35,19 @@ typedef struct {
 // Defines
 #define LPT 0X378
 
+/* Proslic Linefeed options for register 64 - Linefeed Control */
+#define SLIC_LF_OPEN		0x0
+#define SLIC_LF_ACTIVE_FWD	0x1
+#define SLIC_LF_OHTRAN_FWD	0x2
+#define SLIC_LF_TIP_OPEN	0x3
+#define SLIC_LF_RINGING		0x4
+#define SLIC_LF_ACTIVE_REV	0x5
+#define SLIC_LF_OHTRAN_REV	0x6
+#define SLIC_LF_RING_OPEN	0x7
+
+/* Mask used to reverse the linefeed mode between forward and reverse polarity */
+#define SLIC_LF_REVMASK 	0x4
+
 #define IDA_LO  28
 #define IDA_HI	29
 
