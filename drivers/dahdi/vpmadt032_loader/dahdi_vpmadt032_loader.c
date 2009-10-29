cf2444d2836820acb240f10f5c93b4dec2b68299
Shaun Ruffell <sruffell@digium.com>
2009-10-29 18:26:14 +0000

    dahdi_vpmadt032_loader: remove a little dead code.
    git-svn-id: http://svn.asterisk.org/svn/dahdi/linux/trunk@7436 a0bf4364-ded3-4de4-8d8a-66a801d63aff

--- drivers/dahdi/vpmadt032_loader/dahdi_vpmadt032_loader.c
+++ drivers/dahdi/vpmadt032_loader/dahdi_vpmadt032_loader.c
@@ -28,12 +28,6 @@
 
 static int debug;
 
-#define module_printk(level, fmt, args...) \
-	printk(level "%s: " fmt, THIS_MODULE->name, ## args)
-#define debug_printk(level, fmt, args...) if (debug >= level) \
-	printk(KERN_DEBUG "%s (%s): " fmt, THIS_MODULE->name, \
-	__func__, ## args)
-
 #include "voicebus/voicebus.h"
 #include "voicebus/vpmadtreg.h"
 #include "vpmadt032_loader.h"
