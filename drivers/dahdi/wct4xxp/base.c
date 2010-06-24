383c6299767a6fb6e484ccd78c467a414ec716a3
Mike Spiceland <mspiceland@digium.com>
2010-06-24 21:02:56 +0000

    Fix trailing whitespace and make last change more readible. (sruffell suggestion)
    git-svn-id: http://svn.asterisk.org/svn/dahdi/linux/trunk@8805 a0bf4364-ded3-4de4-8d8a-66a801d63aff

--- drivers/dahdi/wct4xxp/base.c
+++ drivers/dahdi/wct4xxp/base.c
@@ -2205,10 +2205,7 @@ static void __t4_configure_t1(struct t4 *wc, int unit, int lineconfig, int txlev
 	if (lineconfig & DAHDI_CONFIG_AMI) {
 		line = "AMI";
 		/* workaround for errata #2 in ES v3 09-10-16 */
-		if (wc->falc31)
-			fmr0 = 0xb0; 
-		else
-			fmr0 = 0xa0;
+		fmr0 = (wc->falc31) ? 0xb0 : 0xa0;
 	} else {
 		line = "B8ZS";
 		fmr0 = 0xf0;
@@ -2301,10 +2298,7 @@ static void __t4_configure_e1(struct t4 *wc, int unit, int lineconfig)
 	if (lineconfig & DAHDI_CONFIG_AMI) {
 		line = "AMI";
 		/* workaround for errata #2 in ES v3 09-10-16 */
-		if (wc->falc31)
-			fmr0 = 0xb0; 
-		else
-			fmr0 = 0xa0;
+		fmr0 = (wc->falc31) ? 0xb0 : 0xa0;
 	} else {
 		line = "HDB3";
 		fmr0 = 0xf0;
