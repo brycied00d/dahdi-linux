48d3aae689c1cfd22d43aed48829a832a9bb3745
Tzafrir Cohen <tzafrir@cohens.org.il>
2010-08-01 16:00:29 +0300

    expose a few functions to be used by sysfs code
    * dahdi_is_sync_master: a new function: allow checking if a span is the
      sync master.
    * sigstr, fill_alarm_str: expose both.

--- drivers/dahdi/dahdi-base.c
+++ drivers/dahdi/dahdi-base.c
@@ -623,6 +623,13 @@ void dahdi_unregister_echocan_factory(const struct dahdi_echocan_factory *ec)
 	write_unlock(&ecfactory_list_lock);
 }
 
+/* Is this span our syncronization master? */
+int dahdi_is_sync_master(const struct dahdi_span *span)
+{
+       return (span == master);
+}
+EXPORT_SYMBOL(dahdi_is_sync_master);
+
 static inline void rotate_sums(void)
 {
 	/* Rotate where we sum and so forth */
@@ -733,7 +740,7 @@ static int dahdi_q_sig(struct dahdi_chan *chan)
 }
 
 #ifdef CONFIG_PROC_FS
-static const char *sigstr(int sig)
+const char *sigstr(int sig)
 {
 	switch (sig) {
 	case DAHDI_SIG_FXSLS:
@@ -780,7 +787,7 @@ static const char *sigstr(int sig)
 	}
 }
 
-static int fill_alarm_string(char *buf, int count, int alarms)
+int fill_alarm_string(char *buf, int count, int alarms)
 {
 	int len;
 
